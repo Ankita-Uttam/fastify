5990cdef7c54a749b89ffa42a2c1380169adeb26
Ankita.uttam <ankita.uttam@IN-Ankitauttam.local>
2019-08-30 15:53:06 +0530

    [Ankita] Add. Display message on naviagting to port

---
+++ fastify-api/src/index.js
@@ -0,0 +1,23 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({
+    logger : true //logger is disabled by default so we are initialising it as true initially
+});
+
+// declare a route
+// async is necessary here otherwise the page just keeps on loading - why??
+fastify.get('/',async (request, reply) => {
+    return { hello: 'world' }
+});
+
+// start the server
+const start = async () => {
+    try {
+        await fastify.listen(3500);
+        fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    } catch (error) {
+        fastify.log.error(error);
+        process.exit(1);
+    }
+};
+
+start();
