e597d48d52957632aee917bc1711508158a53110
Ankita.uttam <ankita.uttam@IN-Ankitauttam.local>
2019-09-06 12:17:55 +0530

    Add. Get users

--- fastify-api/src/index.js
+++ fastify-api/src/index.js
@@ -2,9 +2,32 @@
 const fastify = require('fastify')({
     logger : true //logger is disabled by default so we are initialising it as true initially
 });
+const SQL = require('@nearform/sql');
 
-
+// register : core module of fastify, used to register routes, plugins etc
 fastify.register(require('./routes'));
+
+// register postgres plugin for fastify
+fastify.register(require('fastify-postgres'), {
+    connectionString: 'postgres://ankita.uttam@localhost/sample_user'
+});
+
+fastify.get('/users', (req, reply) => {
+    fastify.pg.connect(onConnect);
+
+    function onConnect (err, client, release) {
+        if (err) return reply.send(err);
+
+        fastify.pg.query(
+            'SELECT * from register_details',
+            function onResult (err, result) {
+                release();
+                const {rows} = result;
+                reply.send(err || rows)
+            }
+        )
+    }
+});
 
 // declare a route
 fastify.get('/',async (request, reply) => {
